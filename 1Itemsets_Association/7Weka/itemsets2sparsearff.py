452318535af44eb3dd841e8f879483f8ba69c547
kz4 <zkcneu@gmail.com>
2017-09-24 23:24:31 -0400

    itemsets2sparsearff done

---
+++ 1Itemsets_Association/7Weka/itemsets2sparsearff.py
@@ -0,0 +1,31 @@
+import sys
+
+def parse(path):
+    readDat(path)
+
+def readDat(path):
+    dataRes = []
+    maxVal = -1
+    with open(path, 'rt') as f:
+        data = f.readlines()
+        for row in data:
+            row = row.replace('\n', '')
+            temp = []
+            for i, featureVal in enumerate(row.split()):
+                pos = int(featureVal)
+                if pos > maxVal:
+                    maxVal = pos
+                temp.append(str(pos-1) + ' 1')
+            dataRowFormat = '{}{}{}'.format('{', ', '.join(temp), '}')
+            dataRes.append(dataRowFormat)
+
+    finalRes = ['@RELATION test']
+    for i in range(maxVal):
+        finalRes.append('@ATTRIBUTE i' + str(i+1) + ' {0, 1}')
+    finalRes.append('@DATA')
+    finalRes += dataRes
+    finalResStr = '\n'.join(finalRes)
+    print(finalResStr)
+
+if __name__ == '__main__':
+    parse(sys.argv[1])
